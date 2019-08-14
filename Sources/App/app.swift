7f1a32e3a6a8dd89d4ca9299cff261305388df8e
Luiz Gustavo Lino <lgustavo@enjoei.com.br>
2019-08-14 19:49:35 -0300

    request printer

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -1,1 +1,15 @@
 import Vapor
+
+
+public struct Store{
+    
+    public let request:Request
+    
+    public init(_ request:Request) {
+        self.request = request
+    }
+    
+    public func render() -> String {
+        return "hello Store requester: \(request)"
+    }
+}
