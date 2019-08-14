59d7883433e8153bb17a3229ef679b8f4f9f8ffc
Luiz Gustavo Lino <lgustavo@enjoei.com.br>
2019-08-14 20:32:40 -0300

    adding store route

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -9,7 +9,15 @@ public struct Store{
         self.request = request
     }
     
-    public func render() -> String {
-        return "hello Store requester: \(request)"
+    public func render() -> EventLoopFuture<String> {
+        let promise = self.request.eventLoop.newPromise(String.self)
+        DispatchQueue.global().async {
+            let store = try! self.request.parameters.next(String.self)
+            let url  = URL(string: "https://www.enjoei.com.br/api/v3/users/\(store)")!
+            let json = try! Data(contentsOf: url)
+            let string = String(data: json, encoding: .utf8)
+            promise.succeed(result: string!)
+        }
+        return promise.futureResult
     }
 }
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -4,25 +4,8 @@ import Vapor
 let app = try Application()
 let router = try app.make(Router.self)
 
-router.get("store") { request in
+router.get("store", String.parameter) { request in
     return Store(request).render()
 }
-
-
-//func getLegacyStore() -> String {
-//    let worker = MultiThreadedEventLoopGroup(numberOfThreads: 2)
-//    let client = try? HTTPClient.connect(hostname: "www.enjoei.com.br", on: worker).wait()
-//    let httpReq = HTTPRequest(method: .GET, url: "/api/v3/users/dixie/")
-//    let httpRes = try? client?.send(httpReq).wait()
-//    return "\(httpRes)"
-//}
-//
-//router.get("store") { req in
-//    func a(){
-//        let promise = req.eventLoop.newPromise(String.self)
-//        print(promise)
-//    }
-//    return getLegacyStore()
-//}
 
 try app.run()
