4079c46545181563762040bc3708c2e942bd2ce8
Luiz Gustavo Lino <lgustavo@enjoei.com.br>
2019-08-14 18:09:23 -0300

    cleaning files

--- Sources/App/routes.swift
+++
@@ -1,9 +0,0 @@
-import Vapor
-
-/// Register your application's routes here.
-public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "olha ae!"
-    }
-}
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,11 @@
 import App
+import Vapor
 
-try app(.detect()).run()
+let app = try Application()
+let router = try app.make(Router.self)
+
+router.get("hello") { req in
+    return "Hello, world! funciona mesmo ae"
+}
+
+try app.run()
