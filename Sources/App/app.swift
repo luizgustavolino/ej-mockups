2ecf7324a7bba39c010dce405ac4b51c1f4ef86f
Luiz Gustavo Lino <lgustavo@enjoei.com.br>
2019-08-14 21:24:41 -0300

    making request using http client

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -1,7 +1,5 @@
 import Vapor
 
-
-
 public struct StoreAPI {
     
     public let request:Request
@@ -14,14 +12,26 @@ public struct StoreAPI {
         let promise = self.request.eventLoop.newPromise(String.self)
         DispatchQueue.global().async {
             do {
+                
+                let client = try HTTPClient.connect(scheme: .https,
+                                                    hostname: "www.enjoei.com.br",
+                                                    on: self.request).wait()
+                
                 let store   = try self.request.parameters.next(String.self)
-                let url     = URL(string: "https://www.enjoei.com.br/api/v3/users/\(store)")!
-                let json    = try Data(contentsOf: url)
-                let legacy  = try JSONDecoder().decode(LegacyStore.self, from: json)
+                let path    = "/api/v3/users/@\(store)"
+                let httpReq = HTTPRequest(method: .GET, url: path)
+                
+                let httpRes = try client.send(httpReq).wait()
+                let data    = httpRes.body.data!
+                
+                let legacy  = try JSONDecoder().decode(LegacyStore.self, from: data)
                 let encoded = try JSONEncoder().encode(legacy.asStore)
                 let encodedJSON = String(data: encoded, encoding: .utf8)
+                
                 promise.succeed(result: encodedJSON!)
+                
             } catch {
+                print("Failing: \(error)")
                 promise.fail(error: error)
             }
         }
